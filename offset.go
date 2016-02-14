026b1a1c6ad4e0b8226d7b9ee8bb152a3ea0526b
Prateek Srivastava <f2prateek@gmail.com>
2016-02-13 16:19:33 -0800

    function args

--- offset.go
+++ offset.go
@@ -4,8 +4,8 @@ import "time"
 
 // Offset returns a clock that always returns a time from the given clock with
 // the specified duration added.
-func Offset(c Clock, d time.Duration) Clock {
+func Offset(c Clock, offset time.Duration) Clock {
 	return ClockFunc(func() time.Time {
-		return c.Now().Add(d)
+		return c.Now().Add(offset)
 	})
 }
