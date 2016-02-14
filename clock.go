576b5bb05b53ff9dda42ffea19beaec1adb38fc1
Prateek Srivastava <f2prateek@gmail.com>
2016-02-13 16:17:13 -0800

    remove defaultin

--- clock.go
+++ clock.go
@@ -12,12 +12,6 @@ func Default() Clock {
 	return ClockFunc(time.Now)
 }
 
-// DefaultIn provides access to the current time from the default clock in the
-// location provided.
-func DefaultIn(loc *time.Location) Clock {
-	return In(Default(), loc)
-}
-
 // The ClockFunc type is an adapter to allow the use of ordinary functions as
 // HTTP handlers. If f is a function with the appropriate signature,
 // ClockFunc(f) is a Clock object that calls f.
